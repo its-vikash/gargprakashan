f02fd0314d443d3efee4c89820b57ee66d09d684
Vikash Singh <72602603+its-vikash@users.noreply.github.com>
2025-06-13 20:15:44 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-red': '#FF2D16',
+        'brand-blue': '#223D4D',
+      },
+    },
+  },
+  plugins: [],
+}
